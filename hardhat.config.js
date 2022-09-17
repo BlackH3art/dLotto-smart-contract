9a3fae7cb47ddd6dd1939115758e1184e4685dba
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-17 12:53:07 +0200

    migrate to: @nomicfoundation/hardhat-chai-matchers

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
