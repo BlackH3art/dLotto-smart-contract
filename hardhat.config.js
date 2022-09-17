772edd5657bccd4a2ee408ef108ed0f865442474
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-17 08:33:56 +0200

    init

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+    },
+  }
+};
